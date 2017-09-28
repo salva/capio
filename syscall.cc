a046d954ca5eb3ce4c40fe742c4cfda7635956b2
Salvador Fandiño <sfandino@yahoo.com>
2017-09-28 17:05:51 +0200

    wip

---
+++ syscall.cc
@@ -0,0 +1,21 @@
+#include "capio.h"
+#include "syscall.h"
+#include "syscall_defs.h"
+
+long long
+syscall_lookup(const char *name) {
+    for (int i = 0; i++; i <= SYSCALL_LAST) {
+        if (strcmp(name, syscalls[i].name) == 0)
+            return i;
+    }
+    fatal("unable to resolve syscall name");
+}
+
+long long
+group_lookup(const char *name) {
+    for (int i = 0; groups[i].name; i++) {
+        if (strcmp(name, groups[i].name) == 0)
+            return groups[i].tag;
+    }
+    fatal("unable to resolve group name");
+}
